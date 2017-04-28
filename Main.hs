c18ebb19761265a3d1397582d1e9022ebe33caf0
Jingwen Chen <jin@crypt.sg>
2017-04-28 22:15:50 +0800

    Added pretty print ansi terminal colours

--- Main.hs
+++ Main.hs
@@ -7,27 +7,36 @@ import System.Environment
 import Parser (parseExpr)
 import Typecheck (typecheckExpr)
 
+ansiGreen :: String 
+ansiGreen = "\x1b[32m"
+
+ansiRed :: String 
+ansiRed = "\x1b[31m" 
+
+ansiReset :: String 
+ansiReset = "\x1b[0m"
+
 process :: String -> IO ()
 process line = case parseExpr line of
   Left err -> print err
   Right ex -> do 
-    putStrLn line
-    print ex
-    putStrLn res
+    putStrLn $ "[Expression]: " ++ line
+    putStrLn $ "[AST]: " ++ show ex
+    putStrLn $ "[Typecheck] " ++ res
     putStrLn ""
       where res = case typecheckExpr ex of 
-                       Left err -> "Typecheck FAIL: " ++ err
-                       Right ty -> "Typecheck OK: " ++ show ty
+                       Left err -> "[" ++ ansiRed ++ "FAIL" ++ ansiReset ++ "]: " ++ show err
+                       Right ty -> "[" ++ ansiGreen ++ "OK" ++ ansiReset ++ "]: " ++ show ty
 
 -- https://hackage.haskell.org/package/haskeline-0.7.3.1/docs/System-Console-Haskeline.html
 runRepl :: IO ()
 runRepl = runInputT defaultSettings loop
   where
-  loop = do
-    minput <- getInputLine "> "
-    case minput of
-      Nothing -> outputStrLn "Goodbye."
-      Just input -> liftIO (process input) >> loop
+    loop = do
+      minput <- getInputLine "> "
+      case minput of
+           Nothing -> outputStrLn "Goodbye."
+           Just input -> liftIO (process input) >> loop
 
 loadFile :: String -> IO ()
 loadFile filePath = do
--- examples/typed_expressions.fun
+++ examples/typed_expressions.fun
@@ -7,6 +7,8 @@ false :: Bool
 { a = 2, b = 3, c = 4 } :: { a: Int, b: Int, c: Int }
 { a = { d = 4 } :: { d : Int }, b = 3, c = 4 } :: { a : Int, b : Int, c : Int, d : Int }
 (fn x :: (Int -> Int) => x)
+(fn x :: (Int -> Int) => 2)
+(fn x :: (Int -> Int) => true)
 (fn x :: ({ a: Int } -> Int) => x) { a = 2 } :: { a: Int }
 (fn x :: ({ a: Int } -> Int) => x.a) { a = 2 } :: { a : Int }
 (fn x :: ({ a: Int } -> Int) => x.a + x.b) { a = 2, b = 3, c = 4 } :: { a : Int, b : Int }
