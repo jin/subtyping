22b593bcc08d69a187afc32dd51f6653a47351aa
Jingwen Chen <jin@crypt.sg>
2017-04-30 01:15:10 +0800

    fixed identation

--- Syntax.hs
+++ Syntax.hs
@@ -4,33 +4,25 @@ import Data.List (intercalate)
 
 data Op = Add | Sub | Mul | Div | GTE | GT | LTE | LT | Equal deriving (Show, Eq)
 
-data Ty = RcdTy [(String, Ty)]
-        | ArrowTy Ty Ty
-        -- | TopTy
-        | IntTy
+data Ty = IntTy
         | BoolTy
-        deriving (Show, Eq)
+        | ArrowTy Ty Ty
+        | RcdTy [(String, Ty)]
+          deriving (Show, Eq)
 
 prettyTy :: Ty -> String
-prettyTy (RcdTy xs) = "{ " ++ prettyRcds xs ++ " }" 
+prettyTy (RcdTy xs) = "{ " ++ prettyRcds xs ++ " }"
   where prettyRcds = intercalate ", " . map (\(lbl, ty) -> lbl ++ ": " ++ prettyTy ty)
 prettyTy (ArrowTy x1 x2) = prettyTy x1 ++ " -> " ++ prettyTy x2
 prettyTy IntTy = "Int"
 prettyTy BoolTy = "Bool"
 
-type Name = String
-
-data Expr =
-  I Int Ty |
-  -- S String |
-  B Bool Ty |
-  Var Name |
-  Fn Name Expr Ty |
-  -- Fun Name Name Expr |
-  FApp Expr Expr |
-  -- Cond Expr Expr Expr |
-  -- Let Expr Expr Expr |
-  BinOp Op Expr Expr |
-  Rcd [(Name, Expr)] Ty |
-  RcdProj Expr Expr
-    deriving (Show, Eq)
+data Expr = I Int Ty
+          | B Bool Ty
+          | Var String
+          | Fn String Expr Ty
+          | FApp Expr Expr
+          | BinOp Op Expr Expr
+          | Rcd [(String, Expr)] Ty
+          | RcdProj Expr Expr
+            deriving (Show, Eq)
